11ab08b66a75311b11d14e116d5343af2790fea1
Naman <naman.bhayana13@gmail.com>
2025-04-29 19:02:09 +0530

    Initial commit with complete Jobzee code

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+// import app from "./app.js";
+// import cloudinary from "cloudinary";
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+//   api_key: process.env.CLOUDINARY_CLIENT_API,
+//   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+// });
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running at port ${process.env.PORT}`);
+// });
+import app from "./app.js";
+app.listen(5000,()=>{
+  console.log("server is runing on port 5000")
+})
